13e6327f44526112ca89d568fd03858d423f7091
Edward Yin <59598698+kaixing2012@users.noreply.github.com>
2021-08-08 15:48:57 +0800

    create user page and widget and service

---
+++ project/lib/pages/user/user_detail.page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:project/shared/models/user.model.dart';
+
+class UserDetailPage extends StatelessWidget {
+  final User user;
+
+  const UserDetailPage({Key key, this.user}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'User List',
+          style: TextStyle(fontSize: 13),
+        ),
+      ),
+      body: Container(
+          padding: EdgeInsets.all(2),
+          child: Container(
+            child: Center(
+              child: Text(user.name),
+            ),
+          )),
+    );
+  }
+}
