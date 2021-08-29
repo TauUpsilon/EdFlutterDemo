472b185704646e3938c188f4cf69edb64121d83b
Edward Yin <59598698+kaixing2012@users.noreply.github.com>
2021-08-29 12:17:10 +0800

    update api service logic

--- project/lib/shared/services/api.service.dart
+++ project/lib/shared/services/api.service.dart
@@ -3,10 +3,11 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:project/main.locator.dart';
 import 'package:project/shared/decorators/imitable.decorator.dart';
-import 'package:project/shared/models/api_response.model.dart';
+import 'package:project/shared/models/api_data.model.dart';
+import 'package:project/shared/models/data_room.model.dart';
 import 'package:project/shared/requests/base.request.dart';
 import 'package:project/shared/services/store.service.dart';
-import 'package:project/store/actions/api_req.action.dart';
+import 'package:project/store/actions/data_request.action.dart';
 import 'package:reflectable/reflectable.dart';
 import 'package:rxdart/rxdart.dart';
 
@@ -14,35 +15,35 @@ class ApiService {
   final _baseUri = 'https://gorest.co.in/public/v1';
   final _storeService = locator.get<StoreService>();
 
-  void request<T>(
-      BaseRequest request, BehaviorSubject<ApiResponse<T>> subject) {
+  BehaviorSubject<ApiData<T>> request<T>(
+      BaseRequest request, Collection<T> collection) {
+    var subject = BehaviorSubject<ApiData<T>>();
     var store = this._storeService.store;
-    var uri = Uri.parse('${this._baseUri}/${request.name}');
+    var uri = Uri.parse('${this._baseUri}/${request.uri}');
 
-    store.dispatch(ApiRequestAction(type: ActionType.LOADING));
-    var status = store.state.apiResponse.status;
-    var res = ApiResponse<T>(status: status);
-    subject.add(res);
+    store.dispatch(DataRequestAction(type: ActionType.LOADING));
 
     http.get(uri).then((res) {
       var mirror = imitable.reflectType(T) as ClassMirror;
 
-      final action = ApiRequestAction(
+      final action = DataRequestAction(
           type: ActionType.SUCCESS,
           payload: jsonDecode(res.body),
+          request: request,
           mirror: mirror);
 
       store.dispatch(action);
 
-      var data = store.state.apiResponse.body.data
+      var data = store.state.dataRoom.collections[request.name].data
           .map<T>((item) => mirror.invoke('fromJson', [item.toJson()]))
           .toList();
 
-      var meta = store.state.apiResponse.body.meta;
-      var status = store.state.apiResponse.status;
-      var body = ResponseBody<T>(meta: meta, data: data);
+      var meta = store.state.dataRoom.collections[request.name].meta;
 
-      subject.add(ApiResponse<T>(status: status, body: body));
+      subject.add(ApiData<T>(
+          store.state.dataRoom.status, Collection<T>(meta: meta, data: data)));
     });
+
+    return subject;
   }
 }
